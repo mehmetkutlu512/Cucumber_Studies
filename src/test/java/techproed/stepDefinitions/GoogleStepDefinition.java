ddad3ede64ea60d1dcf3db1483ee2fbf08b76b1b
Mehmet Kutlu <mehmetkutlu512@gmail.com>
2023-05-11 17:58:22 +0300

    11.05.2023

--- src/test/java/techproed/stepDefinitions/GoogleStepDefinition.java
+++ src/test/java/techproed/stepDefinitions/GoogleStepDefinition.java
@@ -4,6 +4,7 @@ import io.cucumber.java.en.Given;
 import org.junit.Assert;
 import org.openqa.selenium.Keys;
 import techproed.pages.GooglePage;
+import techproed.utilities.ConfigReader;
 import techproed.utilities.Driver;
 
 public class GoogleStepDefinition {
@@ -18,5 +19,17 @@ public class GoogleStepDefinition {
     @Given("basligin {string} icerdigini dogrular")
     public void basligin_icerdigini_dogrular(String string) {
         Assert.assertTrue(Driver.getDriver().getTitle().contains(string));
+    }
+
+    @Given("kullanici googleda {string} aratacaktir")
+    public void kullanici_googleda_aratacaktir(String string) {
+        googlePage = new GooglePage();
+        googlePage.aramaKutusu.sendKeys(ConfigReader.getProperty(string), Keys.ENTER);
+    }
+
+    @Given("basligin {string} icerdigini dogrulatacaktir")
+    public void basligin_icerdigini_dogrulatacaktir(String string) {
+        Assert.assertTrue(Driver.getDriver().getTitle().contains(ConfigReader.getProperty(string)));
+
     }
 }
